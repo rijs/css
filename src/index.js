32377e37b21884aba1465e254895a91bdd3c9d2e
pemrouz <p3pedram@hotmail.com>
2017-06-19 01:50:20 +0100

    feat: calc css hash

--- src/index.js
+++ src/index.js
@@ -6,10 +6,24 @@ export default function css(ripple){
   ripple.types['text/css'] = {
     header: 'text/css'
   , check(res){ return includes('.css')(res.name) }
+  , parse(res){ 
+      res.headers.hash = djb(res.body)
+      return res
+    }
   }
 
   return ripple
 }
 
 import includes from 'utilise/includes'
-var log = require('utilise/log')('[ri/types/css]')
+const log = require('utilise/log')('[ri/types/css]')
+
+const djb = str => {
+  let hash = 5381
+    , i = str.length
+
+  while (i)
+    hash = (hash * 33) ^ str.charCodeAt(--i)
+
+  return hash >>> 0
+}
